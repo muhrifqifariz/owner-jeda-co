8ace0eb8736a28aeb9e27b61115a4aca57e7c474
muhrifqifariz <57094159+muhrifqifariz@users.noreply.github.com>
2026-08-06 17:40:51 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "jeda-owner-v9";
+const CACHE_NAME = "jeda-owner-v11";
 const ASSETS = ["./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png", "./logo-header.png"];
 
 self.addEventListener("install", (event) => {
